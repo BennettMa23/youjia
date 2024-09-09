9e896c2275b57662536527a140167ac545f6bd7e
BennettMa23 <19906406473@163.com>
2024-09-09 21:53:21 +0800

    下拉头像

---
+++ src/components/commonHeader/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+import './index.css'
+
+import { Button, Layout, Menu, Avatar, Dropdown } from "antd";
+
+import { MenuFoldOutlined } from "@ant-design/icons";
+
+const { Header, Content } = Layout;
+
+
+const CommonHeader = () => {
+    const layout = () => {
+        
+    }
+    const items = [
+        {
+          key: '1',
+          label: (
+            <a target="_blank" rel="noopener noreferrer">
+              个人中心
+            </a>
+          ),
+        },
+        {
+          key: '2',
+          label: (
+            <a onClick={() => layout} target="_blank" rel="noopener noreferrer">
+              退出
+            </a>
+          ),    
+        
+        }
+      ];
+      
+
+    return (
+        <Header className="header-container">
+            <Button
+                type="text"
+                // icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                // onClick={() => setCollapsed(!collapsed)}
+                icon={ <MenuFoldOutlined/> }
+                style={{
+                    fontSize: "16px",
+                    width: 64,
+                    height: 32,
+                    background: '#fff',
+                }}
+            />
+            <Dropdown
+                menu={{items}}
+            >
+                <Avatar 
+                src={
+                    <img size={66} src={require("../../assets/images/user.jpg")} />
+                }
+            ></Avatar>
+            </Dropdown>
+            
+            
+        </Header>
+    );
+};
+
+export default CommonHeader;
--- src/pages/main.js
+++ src/pages/main.js
@@ -8,6 +8,7 @@ import {
 } from "@ant-design/icons";
 import { Button, Layout, Menu, theme } from "antd";
 import CommonAsider from "../components/commonAside";
+import CommonHeader from "../components/commonHeader";
 
 const { Header, Sider, Content } = Layout;
 
@@ -49,23 +50,7 @@ const Main = () => {
       </Sider> */}
       <CommonAsider/>
       <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Button
-            type="text"
-            // icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            // onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: "16px",
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
+        <CommonHeader />
         <Content
           style={{
             margin: "24px 16px",
